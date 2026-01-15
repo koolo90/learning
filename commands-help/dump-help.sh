faa42c94217ea703f2717c046198ea59fb647bf6
Tomasz <2711953+koolo90@users.noreply.github.com>
2026-01-15 13:14:49 +0100

    Feature/dump help script (#12)
    * feat(help): added script for dumping help pages from terminal
    
    * feat(help): commands used so far
    
    * feat(help): rewrinting dump script
    
    * chore(utils): added intelij folder to gitignore file
    
    * feat(help): added checking for git branch
    
    * feat(help): added checking for git branch
    
    * feat(help): added debug commands and statements
    
    * feat(help): added debug commands and statements
    
    * feat(help): added debug commands and statements
    
    * feat(help): added debug commands and statements
    
    ---------
    
    Co-authored-by: tomas <gandalf@lord-of-the-lines.com>

--- commands-help/dump-help.sh
+++ commands-help/dump-help.sh
@@ -1,4 +1,49 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
+
+pwd
+pushd .
+
+git_direcory_root_unix_path=$( git rev-parse --show-toplevel | sed "s/C:/\/c/" )
+echo "DEBUG: git_direcory_root=$git_direcory_root_unix_path"
+
+shell_command_name="$1"
+branch_name="feature/$shell_command_name-help-dump"
+help_dir_dump_path="$git_direcory_root_unix_path/commands-help/$shell_command_name"
+help_file_dump_path="$git_direcory_root_unix_path/commands-help/$shell_command_name/help.txt"
+echo "INFO: Checking if command [$shell_command_name] exists..."
+echo "find /usr/bin -name \"$shell_command_name.exe\""
+
+if [[ $(find /usr/bin -name "$shell_command_name.exe") ]]; then
+    echo "DEBUG: Executable for [$shell_command_name] found."
+else
+    echo "ERROR: Executable for [$shell_command_name] not found!"
+    exit -1
+fi
+
+echo "DEBUG: Creating branch for [$shell_command_name], named: [$branch_name]"
+if [[ $(git status --porcelain) ]]; then
+  echo "INFO: git repository poluted with user changes, aborting!"
+  git status
+  exit -2
+else
+  echo "INFO: git repository status is clear, creating branch"
+  echo "DEBUG: Temporarily disabled for development purposes..."
+  git fetch
+  git branch "$branch_name" origin/main
+  git checkout "$branch_name"
+fi
+
+echo "INFO: Creating directory for: [$shell_command_name], path: [$help_dir_dump_path]"
+mkdir -p "$help_dir_dump_path" && ls "$help_dir_dump_path"
+echo "INFO: Creating help file dump for: [$shell_command_name], path: [$help_file_dump_path]"
+touch $help_file_dump_path && ls "$help_file_dump_path"
+echo "INFO: Dumping help page for: [$shell_command_name]"
+bash -c "$shell_command_name --help" >> "$help_file_dump_path"
+
+git add "$help_file_dump_path"
+git commit -m "feat(help): Dumped help for $shell_command_name"
+
+# cat "$help_file_dump_path"
 
 #pwd
 #git status .
@@ -6,19 +51,22 @@
 # git checkout main
 # git pull
 
-shell_command_name="$1"
-echo "INFO: Cleanup: [$shell_command_name]... "
-rm -rf "$shell_command_name"
-git checkout -b "feature/bash-help-$shell_command_name"
+#shell_command_name="$1"
+#echo "INFO: Cleanup: [$shell_command_name]... "
+#rm -rf "$shell_command_name"
+#git checkout -b "feature/bash-help-$shell_command_name"
 
-mkdir -p $shell_command_name
-touch $shell_command_name/help.txt
-bash -c "$shell_command_name --help" > $shell_command_name/help.txt
+#mkdir -p $shell_command_name
+#touch $shell_command_name/help.txt
+#bash -c "$shell_command_name --help" > $shell_command_name/help.txt
 
-git status "./$shell_command_name"
-git add "./$shell_command_name/"
-git status "./$shell_command_name"
+#git status "./$shell_command_name"
+#git add "./$shell_command_name/"
+#git status "./$shell_command_name"
 
-git commit -m "doc(help): Added $shell_command_name help file"
+#git commit -m "doc(help): Added $shell_command_name help file"
 #git push
-git checkout main
+#git checkout main
+
+echo "You can now publish changes by executing $ git push $branch_name"
+popd
