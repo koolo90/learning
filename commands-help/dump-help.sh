c750d1b31d730aa728b8f61f32a3c6cbacaa5164
tomas <gandalf@lord-of-the-lines.com>
2026-01-15 19:18:28 +0100

    refac(help): removed dandling commands in the script

--- commands-help/dump-help.sh
+++ commands-help/dump-help.sh
@@ -45,28 +45,5 @@ git commit -m "feat(help): Dumped help for $shell_command_name"
 
 # cat "$help_file_dump_path"
 
-#pwd
-#git status .
-# git stash
-# git checkout main
-# git pull
-
-#shell_command_name="$1"
-#echo "INFO: Cleanup: [$shell_command_name]... "
-#rm -rf "$shell_command_name"
-#git checkout -b "feature/bash-help-$shell_command_name"
-
-#mkdir -p $shell_command_name
-#touch $shell_command_name/help.txt
-#bash -c "$shell_command_name --help" > $shell_command_name/help.txt
-
-#git status "./$shell_command_name"
-#git add "./$shell_command_name/"
-#git status "./$shell_command_name"
-
-#git commit -m "doc(help): Added $shell_command_name help file"
-#git push
-#git checkout main
-
 echo "You can now publish changes by executing $ git push $branch_name"
 popd
