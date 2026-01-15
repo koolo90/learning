77d3eaebfce6071bb3aa2a9a7a93e7ed14c0d2bd
Tomasz <2711953+koolo90@users.noreply.github.com>
2026-01-15 19:33:47 +0100

    Feature/find help dump (#13)
    * feat(help): Dumped help for find
    
    * feat(help): added more verbose output, removed dandling commits
    
    ---------
    
    Co-authored-by: tomas <gandalf@lord-of-the-lines.com>

--- commands-help/dump-help.sh
+++ commands-help/dump-help.sh
@@ -43,30 +43,10 @@ bash -c "$shell_command_name --help" >> "$help_file_dump_path"
 git add "$help_file_dump_path"
 git commit -m "feat(help): Dumped help for $shell_command_name"
 
-# cat "$help_file_dump_path"
-
-#pwd
-#git status .
-# git stash
-# git checkout main
-# git pull
-
-#shell_command_name="$1"
-#echo "INFO: Cleanup: [$shell_command_name]... "
-#rm -rf "$shell_command_name"
-#git checkout -b "feature/bash-help-$shell_command_name"
-
-#mkdir -p $shell_command_name
-#touch $shell_command_name/help.txt
-#bash -c "$shell_command_name --help" > $shell_command_name/help.txt
-
-#git status "./$shell_command_name"
-#git add "./$shell_command_name/"
-#git status "./$shell_command_name"
-
-#git commit -m "doc(help): Added $shell_command_name help file"
-#git push
-#git checkout main
-
-echo "You can now publish changes by executing $ git push $branch_name"
+echo "You can now publish changes by executing: [$ git push $branch_name]"
+echo "\t git push $branch_name"
+echo "File is stored in dir: [$help_dir_dump_path]"
+echo "Content of grabbed help file for [$shell_command_name] is: "
+echo "=== [$help_file_dump_path] ==="
+echo "=== EOF: [$help_file_dump_path] ==="
 popd
